72a407f59f145d35e8015e583e1ddcc0f409c2fe
Ryan Elmore <rtelmore@gmail.com>
2016-06-13 15:50:27 -0600

    read/process data

--- src/load.R
+++ src/load.R
@@ -1,1 +1,15 @@
 ## Ryan Elmore
+## 13 Jun 2016
+
+library(readr)
+library(lubridate)
+library(dplyr)
+
+df <- read_csv("data/311_service_data_2015.csv")
+
+df$`Case Created Date` <- mdy(df$`Case Created Date`)
+
+df_jan <- df %>% 
+  filter(`Case Created Date` < ymd("2015-02-01")) 
+saveRDS(df_jan, file = "data/jan_data.rds")
+
