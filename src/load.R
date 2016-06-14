5970cd8b796f1b63686112d5573a97ab63bdb8fe
Ryan Elmore <rtelmore@gmail.com>
2016-06-14 13:07:17 -0600

    seed

--- src/load.R
+++ src/load.R
@@ -8,8 +8,12 @@ library(dplyr)
 df <- read_csv("data/311_service_data_2015.csv")
 
 df$`Case Created Date` <- mdy(df$`Case Created Date`)
+saveRDS(df, file = "data/311_service_data_2015.rds")
 
 df_jan <- df %>% 
   filter(`Case Created Date` < ymd("2015-02-01")) 
 saveRDS(df_jan, file = "data/jan_data.rds")
 
+set.seed(10830)
+df_sample <- sample_frac(df, size = .1)
+saveRDS(df_sample, file = "data/311_service_data_2015_sample.rds")
